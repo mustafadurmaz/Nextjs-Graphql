0a1f5d0ad6b04c99f292a541640b4bb8bacbb385
mustafadurmaz <mdmz_001@hotmail.com>
2023-08-08 00:02:53 +0300

    primereact installed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
+
+import { Button } from 'primereact/button';
+
+import "primeflex/primeflex.css";
+import "primeicons/primeicons.css";
+import "primereact/resources/primereact.min.css";
+import 'primereact/resources/themes/saga-blue/theme.css';
+
 
 export default function Home() {
   return (
     <div>
-      test
+      <Button label="Submit" />
     </div>
   )
 }
